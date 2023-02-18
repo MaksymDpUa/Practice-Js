148ff56386488e8931dc3632dcf51be0fa541e94
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-18 16:55:06 +0200

    Додано задачі 8 і 9 для ДЗ

--- js/lesson1.js
+++ js/lesson1.js
@@ -2,11 +2,9 @@
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА,
 // МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-
 // const firstName = "Галина";
 // const age = 35;
 // const course = "JAVASCRIPT";
-
 
 // let message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${firstName}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${course}`;
 // console.log(message);
@@ -14,7 +12,6 @@
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ, ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ
 // "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
-
 
 // const firstName = prompt("Ваше ім'я").toUpperCase();
 // const telefon = prompt("Ваш телефон");
@@ -30,12 +27,12 @@
 // const sum3 = +prompt("Введіть ціну"); //В старих версіях використовують "+", але це погана практика!
 
 // let total = sum1 + sum2 + sum3;
-// console.log(total); 
+// console.log(total);
 
 // ЗАДАЧА 4
-// ВИКОРИСТОВУЮЧИ КОНСТРУКЦІЮ IF...ELSE , НАПИШИ КОД, ЯКИЙ БУДЕ ЗАПИТУВАТИ: 
+// ВИКОРИСТОВУЮЧИ КОНСТРУКЦІЮ IF...ELSE , НАПИШИ КОД, ЯКИЙ БУДЕ ЗАПИТУВАТИ:
 // "СКІЛЬКИ МІСЯЦІВ МАЄ РІК", ЯКЩО КОРИСТУВАЧ ВВОДИТЬ 12 ТО У КОНСОЛІ ПИШЕ ВІРНО,
-//  ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ 
+//  ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ
 // "НЕ ЗНАЄТЕ? 12"
 
 // const yers = Number(prompt('Кількість місяців має рік'));
@@ -47,7 +44,7 @@
 // }
 
 // ЗАДАЧА 5
-// НАПИШІТЬ ПРОГРАМУ, ЯКА ДОЗВОЛЯТИМЕ ЮЗЕРУ ВВОДИТИ ЧИСЛО І 
+// НАПИШІТЬ ПРОГРАМУ, ЯКА ДОЗВОЛЯТИМЕ ЮЗЕРУ ВВОДИТИ ЧИСЛО І
 // ОПРИДІЛЯТИМЕ ЧИ ЦІЛЕ ЦЕ ЧИСЛО, ЧИ НЕ ЦІЛЕ
 
 // const num = Number(prompt('Введіть число'));
@@ -65,7 +62,7 @@
 
 // if (mon === 12 || mon === 1 || mon === 2) {
 // 	console.log('зима');
-// } else if (mon >= 3 && mon <= 5){ 
+// } else if (mon >= 3 && mon <= 5){
 //  console.log('весна');
 // } else if (mon >= 6 && mon <= 8) {
 // 	console.log('літо');
@@ -102,11 +99,11 @@
 // console.log(styles);
 
 // ЗАДАЧА 2
-// Напишіть ф-цію logItems(array) , яка буде получати масив і використовуючи цикл for , який для кожного елемента масива буде виводити в консоль 
+// Напишіть ф-цію logItems(array) , яка буде получати масив і використовуючи цикл for , який для кожного елемента масива буде виводити в консоль
 // повідомлення у форматі <індекс елемента> та <значення елемента>.
 // Номерація для кожного елемента масива ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено 1 - Mango, а для индекса 2 выведет 3 - Ajax.
 
-// const fruits = ['Mango', 'Poly', 'Ajax']; 
+// const fruits = ['Mango', 'Poly', 'Ajax'];
 
 // // function logItems(array) {
 // // for (let i = 0; i < fruits.length; i++) {
@@ -125,7 +122,7 @@
 // 1) напишіть циклом for
 // const numbers = [2, 17, 94, 1, 23, 37];
 // 2) перепишіть функцією findSmallestNumber(numbers)
-// // console.log(findSmallestNumber([2, 17, 94, 1, 23, 37])); 
+// // console.log(findSmallestNumber([2, 17, 94, 1, 23, 37]));
 // // console.log(findSmallestNumber([49, 4, 83, 7, 12]));
 
 // const numbers = [2, 17, 94, 1, 23, 37];
@@ -152,3 +149,76 @@
 
 // console.log(findSmallestNumber([49, 4, 83, 7, 12]));
 // console.log(findSmallestNumber([2, 17, 94, 1, 23, 37]));
+
+//ЗАДАЧА 4
+//Провірте , чи являється рядок полідромом
+
+// const str = "ротатор"; //true
+// const newString = "Hello"; //false
+
+// const splitStr = str.split("").reverse().join("");
+
+// // const splitStr = newString.split("");
+// // console.log(splitStr);
+// // splitStr.reverse();
+
+// // const reversStr = splitStr.join('');
+
+// console.log(str === splitStr);
+
+// ЗАДАЧА 5
+// Напишіть ф-цію calculateAverage() яка приймає довільну кількість аргументів і повертає їх середнє значення .Всі аргументи будуть тільки числовими
+
+// function calculateAverage() {
+//     const arr = Array.from(arguments);
+//     let averrage = 0;
+
+//     for (let num of arr) {
+//         averrage += num;
+//     }
+
+//     return averrage / arr.length;
+//     //console.log(arr);
+// }
+
+// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calculateAverage(14, 8, 2)); // 8
+// console.log(calculateAverage(27, 43, 2, 8, 36));// 23.
+
+// ЗАДАЧА 6
+// Напишіть ф-цію greet(name) , яка при виклику буде получати імя (як приклад , "Василь"), і логінувати рядок "Привіт, <імя>".У випадку , відсутнього
+// відсутнього значення, виводе аргумент "Привіт , гість"
+
+// function greet(name = 'гість') {
+//     return `Привіт, ${name}`;
+// }
+
+//  console.log(greet("Василь"));//Привіт Василь
+//  console.log(greet());//Привіт гість
+
+
+
+// ЗАДАЧА 8
+// Напишіть ф - цію capitalize, яка буде приймати рядок і буде повертати новий рядок, де кожне слово буде з великої букви
+
+
+
+// ЗАДАЧА 9
+// Напишіть функцію для роботи з колекцією навчаючих курсів courses:
+
+// addCourse(name) - добавляє курс у кінець колекції
+// removeCourse(name) - видаляє курс із колекції
+// updateCourse(oldName, newName)- змінює імя на нове
+// const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+
+// // addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+// // addCourse("CSS"); // 'У вас уже є такий курс'
+// // removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+// // removeCourse("Vue"); // 'Курс з таким іменем не найдено'
+// // updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+// console.log(courses);
+
+// //function
+// // capitalize('the quick brown fox')// 'The Quick Brown Fox '
+
+
