bec6283c8f33fcc7a024dd1c34c4ff344de26b61
Alla <urt2509@gmail.com>
2023-03-04 15:03:00 +0100

    Update lesson2.js

--- js/lesson2.js
+++ js/lesson2.js
@@ -31,7 +31,6 @@
 // Повинно получитись 390, якщо обєкт salaries пустий , то результат буде 0
 // ЩОБ МОЖНА БУЛО ПЕРЕВИКОРИСТОВУВАТИ ЛОГІКУ, ТОМУ ПИШЕМО ФУНКЦІЮ
 
-
 //  const salaries = {
 //    John: 100,
 //    Ann: 160,
@@ -43,7 +42,7 @@
 //         total += salaries[key];
 //     }
 //     return total;
-    
+
 // }
 
 // function totalSalary(salaries) {
@@ -81,7 +80,6 @@
 // 	},
 // };
 
-
 // calculator.read();
 // console.log(`sum`, calculator.sum());
 // console.log(`mult`, calculator.mult());
@@ -90,17 +88,16 @@
 // letMeSeeYourName(callback) - запитує імя користувача, через prompt і викликає колбек функцію(callback)
 // greet(name) - колбек, який приймає імя і логінує в консоль рядок  "Привіт" + імя
 
-// function letMeSeeYourName(callback) 
+// function letMeSeeYourName(callback)
 // {
 //     let name = prompt("Назвіть своє ім'я");
 //     callback(name);
-    
+
 // }
 // function greet(name) {
 //     console.log(`Привіт, ${name}`);
 // }
 // letMeSeeYourName(greet);
-
 
 // ЗАДАЧА 2
 // Напишіть дві функції:
@@ -118,3 +115,106 @@
 // };
 
 // makeProduct("Холодильник", 100, showProduct);
+// ЗАДАЧА 3
+// МАЄМО МАСИВ, ПОТРІБНО:
+// 1)получити масив вчених які народились у 19ст;
+// 2)порахувати загальну суму років скільки прожили вчені , і визначити середнє значення;
+// 3) відсортувати по алфавіту;
+// 4)відсортувати по кількості прожитих років;
+// 5)відфільтрувати, хто народився в 15,16,17ст;
+// 6)знайти рік народження Albert Einstein;
+// 7)визначити, чи усі вчені працювали у 19ст;
+
+const scientists = [
+  {
+    name: "Albert",
+    surname: "Einstein",
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: "Isaac",
+    surname: "Newton",
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: "Galileo",
+    surname: "Galilei",
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: "Marie",
+    surname: "Curie",
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: "Johannes",
+    surname: "Kepler",
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: "Nicolaus",
+    surname: "Copernicus",
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: "Max",
+    surname: "Planck",
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: "Katherine",
+    surname: "Blodgett",
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: "Ada",
+    surname: "Lovelace",
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+
+  {
+    name: "Lise",
+    surname: "Meitner",
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: "Sarah E.",
+    surname: "Goode",
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: "Hanna",
+    surname: "Hammarström",
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
+// // 1)получити масив вчених які народились у 19ст;
+
+// const arr = scientists.filter((el) => el.born >= 1800 && el.born < 1900);
+// const arrNames = arr.map(({ name, surname }) => `${name}, ${surname}`);
+// console.log(arr);
+// console.log(arrNames);
