4b85c5126873137188df550a5880717e20017153
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-03-04 14:39:51 +0200

    Виконано завдання 1

---
+++ js/lesson2.js
@@ -0,0 +1,26 @@
+// ЗАДАЧА 1
+// Напишіть скрипт,для обєкта user в послідовності:
+
+//  добавляє поле mood із значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення  premium на false
+// виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
+
+//  const user = {
+//    name: "Mango",
+//    age: 20,
+//    hobby: "html",
+//    premium: true,
+// };
+
+// user.mood = 'happy';
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const keys = Object.keys(user);
+
+// for (const key of keys) {
+//     console.log(key, " : ", user[key]);
+// }
+
+// console.log(user);
