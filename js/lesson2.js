6b13fab4641b484752bc4a7b3beb298f1710c902
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-03-18 13:58:49 +0100

    Update lesson2.js

--- js/lesson2.js
+++ js/lesson2.js
@@ -125,93 +125,93 @@
 // 6)знайти рік народження Albert Einstein;
 // 7)визначити, чи усі вчені працювали у 19ст;
 
-const scientists = [
-  {
-    name: "Albert",
-    surname: "Einstein",
-    born: 1879,
-    dead: 1955,
-    id: 1,
-  },
-  {
-    name: "Isaac",
-    surname: "Newton",
-    born: 1643,
-    dead: 1727,
-    id: 2,
-  },
-  {
-    name: "Galileo",
-    surname: "Galilei",
-    born: 1564,
-    dead: 1642,
-    id: 3,
-  },
-  {
-    name: "Marie",
-    surname: "Curie",
-    born: 1867,
-    dead: 1934,
-    id: 4,
-  },
-  {
-    name: "Johannes",
-    surname: "Kepler",
-    born: 1571,
-    dead: 1630,
-    id: 5,
-  },
-  {
-    name: "Nicolaus",
-    surname: "Copernicus",
-    born: 1473,
-    dead: 1543,
-    id: 6,
-  },
-  {
-    name: "Max",
-    surname: "Planck",
-    born: 1858,
-    dead: 1947,
-    id: 7,
-  },
-  {
-    name: "Katherine",
-    surname: "Blodgett",
-    born: 1898,
-    dead: 1979,
-    id: 8,
-  },
-  {
-    name: "Ada",
-    surname: "Lovelace",
-    born: 1815,
-    dead: 1852,
-    id: 9,
-  },
+// const scientists = [
+//   {
+//     name: "Albert",
+//     surname: "Einstein",
+//     born: 1879,
+//     dead: 1955,
+//     id: 1,
+//   },
+//   {
+//     name: "Isaac",
+//     surname: "Newton",
+//     born: 1643,
+//     dead: 1727,
+//     id: 2,
+//   },
+//   {
+//     name: "Galileo",
+//     surname: "Galilei",
+//     born: 1564,
+//     dead: 1642,
+//     id: 3,
+//   },
+//   {
+//     name: "Marie",
+//     surname: "Curie",
+//     born: 1867,
+//     dead: 1934,
+//     id: 4,
+//   },
+//   {
+//     name: "Johannes",
+//     surname: "Kepler",
+//     born: 1571,
+//     dead: 1630,
+//     id: 5,
+//   },
+//   {
+//     name: "Nicolaus",
+//     surname: "Copernicus",
+//     born: 1473,
+//     dead: 1543,
+//     id: 6,
+//   },
+//   {
+//     name: "Max",
+//     surname: "Planck",
+//     born: 1858,
+//     dead: 1947,
+//     id: 7,
+//   },
+//   {
+//     name: "Katherine",
+//     surname: "Blodgett",
+//     born: 1898,
+//     dead: 1979,
+//     id: 8,
+//   },
+//   {
+//     name: "Ada",
+//     surname: "Lovelace",
+//     born: 1815,
+//     dead: 1852,
+//     id: 9,
+//   },
 
-  {
-    name: "Lise",
-    surname: "Meitner",
-    born: 1878,
-    dead: 1968,
-    id: 11,
-  },
-  {
-    name: "Sarah E.",
-    surname: "Goode",
-    born: 1855,
-    dead: 1905,
-    id: 10,
-  },
-  {
-    name: "Hanna",
-    surname: "Hammarström",
-    born: 1829,
-    dead: 1909,
-    id: 12,
-  },
-];
+//   {
+//     name: "Lise",
+//     surname: "Meitner",
+//     born: 1878,
+//     dead: 1968,
+//     id: 11,
+//   },
+//   {
+//     name: "Sarah E.",
+//     surname: "Goode",
+//     born: 1855,
+//     dead: 1905,
+//     id: 10,
+//   },
+//   {
+//     name: "Hanna",
+//     surname: "Hammarström",
+//     born: 1829,
+//     dead: 1909,
+//     id: 12,
+//   },
+// ];
 // // 1)получити масив вчених які народились у 19ст;
 
 // const arr = scientists.filter((el) => el.born >= 1800 && el.born < 1900);
@@ -254,7 +254,7 @@ const scientists = [
 
 // const scient = scientists.find(({name, surname}) => name === "Albert" && surname === "Einstein");
 
-// console.log(scient.born) 
+// console.log(scient.born)
 
 
 
@@ -319,3 +319,26 @@ const scientists = [
 //     ],
 //   },
 // ];
+
+function DNAStrand(dna){
+  const arr = [...dna].reduce((acc, item) => {
+    if(item === "A") {
+      acc += "T";
+      return acc
+    } else if(item === "T") {
+      acc += "A";
+       return acc
+    }
+    else if(item === "G") {
+      acc += "C";
+       return acc
+    }
+    else if(item === "C") {
+      acc += "G";
+       return acc
+    }
+  }, "" )
+return acc
+  console.log(arr);
+}
+console.log(DNAStrand("AAAAA"));
