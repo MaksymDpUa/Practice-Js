65363eeda6aa231eb97486a5da44ccf38725924f
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-04-01 17:00:42 +0300

    Додано умову для задачі 4 (AutoRia)
    ДЗ

--- js/lesson4.js
+++ js/lesson4.js
@@ -133,3 +133,61 @@
 //     addEventListener("scroll", scrollEv);
 
 // console.log(headerEl.clientHeight);
+
+
+// ЗАДАЧА 4
+// ПОТРІБНО СТВОРИТИ Ф-ЦІЮ , ЯКА БУДЕ ШУКАТИ НАРЦИСА.ГОЛОВНА УМОВА НАРЦИСА ЗНАЮТЬ УСІ , НАРЦИС НЕ ЗНАЄ НІКОГО
+// // є елемент масиву , який незнає нікого
+// // якщо не одна така людина , то вертаємо not found
+// // якщо один , то перевірити , чи його знають усі люди, якщо так то вертаємо found
+
+// const people1 = [{
+//   name: 'Alex',
+//   know: ["Eva", "Jhon"],
+// },
+// {
+//   name: 'Ivan',
+//   know: ["Jhon", "Alex"],
+// },
+// {
+//   name: 'Eva',
+//   know: ["Alex", "Jhon"],
+// },
+// {
+//   name: 'Jhon',
+//   know: [],
+// }]
+
+// const people2 = [{
+//   name: 'Alex',
+//   know: ["Eva", "Jhon"],
+// },
+// {
+//   name: 'Jhon',
+//   know: ["Eva"],
+// },
+// {
+//   name: 'Eva',
+//   know: [],
+// },
+// {
+//   name: 'Ivan',
+//   know: ["Jhon", "Alex"],
+// }]
+
+// const people3 = [{
+//   name: 'Alex',
+//   know: ["Eva", "Jhon"],
+// },
+// {
+//   name: 'Jhon',
+//   know: [],
+// },
+// {
+//   name: 'Eva',
+//   know: ["Alex", "Jhon"],
+// },
+// {
+//   name: 'Ivan',
+//   know: ["Jhon", "Alex"],
+// }]
