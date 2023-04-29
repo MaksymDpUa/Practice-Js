1bf20f5c1ca4e200487ecd7918b3a4ba1aa55f26
Oleksandra Andrushchenko <oleksandraandrusenko6@gmail.com>
2023-04-29 16:59:12 +0300

    task3_2

--- js/lesson6.js
+++ js/lesson6.js
@@ -72,44 +72,82 @@
 //   </div>
 // fetch(`https://api.github.com/search/users?q=${}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c`)
 
-const form = document.querySelector("#form");
-const input = document.querySelector("#input");
-const divUser = document.querySelector(".js-user");
-const btnNestPage = document.querySelector(".js-btn");
-let pageItems = 1;
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const divUser = document.querySelector(".js-user");
+// const btnNestPage = document.querySelector(".js-btn");
+// let pageItems = 1;
 
-form.addEventListener("submit", onInput);
+// form.addEventListener("submit", onInput);
 
-function onInput(evt) {
-  evt.preventDefault();
-  const inputValue = input.value;
+// function onInput(evt) {
+//   evt.preventDefault();
+//   const inputValue = input.value;
 
-  fetch(
-    `https://api.github.com/search/users?q=${inputValue}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&page=${pageItems}&per_page=5`
-  )
-    .then((resp) => resp.json())
-    .then((data) => {
-      if (data.items.length > 0) {
-        createList(data.items);
-        pageItems++;
-      } else {
-        alert("The end");
-      }
-    })
-    .catch((err) => console.log(err.message));
-}
+//   fetch(
+//     `https://api.github.com/search/users?q=${inputValue}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&page=${pageItems}&per_page=5`
+//   )
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//       if (data.items.length > 0) {
+//         createList(data.items);
+//         pageItems++;
+//       } else {
+//         alert("The end");
+//       }
+//     })
+//     .catch((err) => console.log(err.message));
+// }
 
-function createMarkup({ avatar_url, login }) {
-  const markup = `<div>
-    <img src="${avatar_url}" alt="${login}">
-    <h2>${login}</h2>
-  </div>`;
-  divUser.insertAdjacentHTML("beforeend", markup);
-}
+// function createMarkup({ avatar_url, login }) {
+//   const markup = `<div>
+//     <img src="${avatar_url}" alt="${login}">
+//     <h2>${login}</h2>
+//   </div>`;
+//   divUser.insertAdjacentHTML("beforeend", markup);
+// }
 
-function createList(arr) {
-  arr.forEach((item) => createMarkup(item));
-}
+// function createList(arr) {
+//   arr.forEach((item) => createMarkup(item));
+// }
 
-btnNestPage.addEventListener("click", onInput);
+// btnNestPage.addEventListener("click", onInput);
 
+// ЗАДАЧА 3
+// НАПИШІТЬ ПАГІНАЦІЮ, ДЛЯ ПЕРЕХОДУ ПО СТОРІНКАХ
+// https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-user
+//  <div id='root'>
+//     <form id='form'>
+//         <input type="text" id="input">
+//       <button>Пошук</button>
+//     </form>
+//   </div>
+// fetch(`https://api.github.com/search/users?q=${}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c`)
+
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const divUser = document.querySelector(".js-user")
+
+// form.addEventListener('submit', onInput);
+
+// function onInput(e){
+// 	e.preventDefault();
+// 	const inputValue = input.value;
+// 	console.log(inputValue);
+// 	fetch(`https://api.github.com/search/repositories?q=${inputValue}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c`)
+// 		.then(resp => resp.json())
+// 		.then(data => createList(data.items))
+// 		.catch(err => console.log(err));
+// }
+ 
+// function createMarkup({ clone_url, owner: { login } }) {
+//   const markup = `<div>
+//     <a href="${clone_url}">Посилання</a>
+//     <h2>${login}</h2>
+//   </div>`;
+//   divUser.insertAdjacentHTML("beforeend", markup);
+// }
+
+// function createList(arr) {
+//   arr.forEach((item) => createMarkup(item));
+// }
